00961c60cb7226f6b82ad4a54af69604a464b782
GordonWoon <gordonwoon@live.com>
2019-11-12 02:07:49 +0800

    add graphql support

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import Navigation from './components/navigation';
 import Dashboard from './components/dashboard';
 import Planner from './components/planner';
 
+const client = new ApolloClient({
+  uri: 'http://travelplannerservereb-env.ynnyx3isx3.ap-southeast-1.elasticbeanstalk.com/graphql',
+});
+
 const App = () => {
   return (
-    <Router>
-      <Navigation>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route path="/planner" component={Planner} />
-        </Switch>
-      </Navigation>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navigation>
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/planner" component={Planner} />
+          </Switch>
+        </Navigation>
+      </Router>
+    </ApolloProvider>
   );
 }
 

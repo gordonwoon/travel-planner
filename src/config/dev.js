e9e1d10feb11433d9da5c09efa469be751e9fc12
Gordon Woon <gordonwoon@live.com>
2019-11-17 23:34:25 +0800

    [Fix] remove keys (#8)
    * remove keys
    
    * ignore keys

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  googleAPIKey: 'AIzaSyDvD657OY9wY-YbGjtjWoUQ4AleQ3wV0GM',
-  serverURL: 'http://travelplannerservereb-env.ynnyx3isx3.ap-southeast-1.elasticbeanstalk.com'
-  //serverURL: 'http://localhost:5000'
-};
